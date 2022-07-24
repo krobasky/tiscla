ebced97cf2898f5926ef4ca7b57ab4ebd499d6d2
Kimberly Robasky <krobasky@gmail.com>
2022-07-24 19:25:06 -0400

    more code doc

--- src/sbr/layers.py
+++ src/sbr/layers.py
@@ -4,12 +4,10 @@ from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Dropo
 
 from keras import activations
 
-import absl.logging
-absl.logging.set_verbosity(absl.logging.ERROR)
 # suppress this warning:
 # WARNING:absl:Found untraced functions such as ...
-# BUT now we have this in sbr.model.save:
-# <class 'ValueError'>: Unknown layer: BADBlock. Please ensure this object is passed to the `custom_objects` argument. See https://www.tensorflow.org/guide/keras/save_and_serialize#registering_the_custom_object for details.
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
 
 class BADBlock(layers.Dense):
     '''
@@ -30,15 +28,22 @@ class BADBlock(layers.Dense):
     from tensorflow.keras.models import Sequential
     from sbr.layers import BADBlock
     model = Sequential()
-    model.add(BADBlock(1000, input_dim = 18963, name="BAD_1"))
+    model.add(BADBlock(1000, input_dim = 18963, activation='relu', dropout_rate=0.50, name="BAD_1"))
     model.add(Dense(26, activation="softmax"))
     model.summary()
     model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy','mse'])
     '''
-
     def __init__(self, units,
                  activation = 'relu',
-                 proxy_activation = None, # need to rename activation so Dense doesn't use it, but you need an argument to save in config or config won't work
+                 # need to rename activation and set it to 'None' so Dense doesn't use it, 
+                 # but still want user to be able to use reactivation, 
+                 # but need an argument to __init__ so get_config can use and save it in the model.save function
+                 # so...
+                 # make another argument 'proxy_activation' and hope the user doesn't call it, 
+                 # then override 'proxy_activation' value with the value of the 'activation' argument above
+                 # that way you can keep the self.activation = None, but you still have proxy_activation for get_config
+                 # There's got to be a better way this is an ugly kludge!
+                 proxy_activation = None, 
                  dropout_rate = 0.50, 
                  trainable= True,
                  **kwargs
